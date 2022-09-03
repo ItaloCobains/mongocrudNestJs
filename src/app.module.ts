0008a4c4e2fd5baa5495edc589f203db69890baa
ItaloCobains <monokatchaleger@gmail.com>
2022-09-03 18:12:55 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  controllers: [AppController],
+  providers: [AppService, AuthService],
+})
+export class AppModule {}
---
+++ src/users/dto/signup.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export class SignupDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  password: string;
+}
